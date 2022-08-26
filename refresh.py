4b3a1879cd27db4bedf99eef93656090d87ebfce
Matt Lavis <matthew.lavis@gmail.com>
2022-08-26 18:58:25 +0100

    Test refresh

--- refresh.py
+++ refresh.py
@@ -2,6 +2,6 @@ import classes.globals as g
 from classes_gen.database import Database
 
 
-sql = "REFRESH MATERIALIZED VIEW utils.materialized_measures_real_end_dates"
+sql = "REFRESH  MATERIALIZED VIEW utils.materialized_measures_real_end_dates"
 d = Database()
 d.run_query(sql)
